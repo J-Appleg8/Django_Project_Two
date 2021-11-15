e82611357a0e434eae387f5873f965fe7b28f66c
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-11-14 16:31:26 -0800

    finished first models lecture

--- second_app/admin.py
+++ second_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from second_app.models import Topic, Webpage, AccessRecord
 
-# Register your models here.
+# Password: testpassword123
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
